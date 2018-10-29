cc40f4ee3e0c4af3694c4e4e1d42400438d05e11
Zhihao Zhao <37906196+zhaozha@users.noreply.github.com>
2018-10-29 23:48:15 +0800

    201810292348

--- tubushu-backend-login/src/main/java/com/tubushu/UserProvider8001_App.java
+++ tubushu-backend-login/src/main/java/com/tubushu/UserProvider8001_App.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication(scanBasePackages = "com")
 @MapperScan("com.tuboshu.dao")
 public class UserProvider8001_App{
